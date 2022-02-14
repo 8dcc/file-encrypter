c8b2ada047b4fcde6082c86b19acbf06d1ebe34d
ravi0li <29655971+r4v10l1@users.noreply.github.com>
2022-02-14 20:11:00 +0100

    v1.3

---
+++ src/main.c
@@ -0,0 +1,39 @@
+/*
+ * https://github.com/r4v10l1/file-encrypter
+ */
+
+#include <stdio.h>
+#include "settings.h"
+#include "hashes.h"
+#include "encrypt.h"
+#include "decrypt.h"
+
+int main() {
+	char mode;
+
+	printf("[E/D] Select mode: ");
+	mode = getchar();
+	
+	switch (mode) {
+		case 'E':
+			encrypt();
+			break;
+		case 'e':
+			encrypt();
+			break;
+		case 'D':
+			decrypt();
+			break;
+		case 'd':
+			decrypt();
+			break;
+		default:
+			printf("\nWrong option...\n");
+			return 1;
+			break;	// lol
+	}
+
+	return 0;
+}
+
+
