d3888555bf34e3e6953474c44172440ece1e17a9
8dcc <8dcc.git@gmail.com>
2022-12-14 20:08:00 +0100

    Add test code to encrypt_file

--- src/encrypt.c
+++ src/encrypt.c
@@ -4,8 +4,21 @@
  * https://github.com/r4v10l1/file-encrypter
  */
 
+#include <stdio.h>
+
+#include "lib/aes256.h"
+
 void encrypt_file(char* filename) {
+    // 32 bytes (256 bits)
+    const char key[] = "0123456789"
+                       "0123456789"
+                       "0123456789"
+                       "01";
+
+    printf("Using key: \"%s\"\n", key);
+    printf("Encrypting text: \"%s\"\n", filename);
+
+    // TODO: Paddings to data and key, xor excess key with itself
 }
 
-void decrypt_file(char* filename) {
-}
+void decrypt_file(char* filename) {}
